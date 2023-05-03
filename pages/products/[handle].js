02088401c190f369208dfa5aaaa3a682196f4a19
rshaki01 <rshaki01@gmail.com>
2023-05-03 08:35:03 -0400

    implemented handle.js and dynamic product pages

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -16,28 +16,68 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params}) {
-    const products = await fetchProducts();
+    const response = await fetch(
+        'https://faux-nostalgia-1.myshopify.com/api/2023-04/graphql.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Shopify-Storefront-Access-Token': process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+          },
+          body: JSON.stringify({
+            query: `
+            query($handle: String!) {
+              product(handle: $handle) {
+                id
+                title
+                description
+                images(first: 1) {
+                  edges {
+                    node {
+                      url
+                    }
+                  }
+                }
+              }
+            }`
+            ,
+            variables : {
+              'handle': params.handle
+            },
+          }),
+        }
+      );
+    
+      const { data } = await response.json();
+      console.log(data)
+
+      const product = {
+        id: 1,
+        title: data.product.title,
+        description: data.product.description,
+        imageSrc: data.product.images.edges[0].node.url
+      };
+
     return {
       props: {
-        products,
+        product,
       },
       revalidate: 1,
     }
 }
 
-const productPage = ({products, params}) => {
-
+const productPage = ({product}) => {
 
   const [quantity, setQuantity] = useState(1);
 
   return (
     <div className='flex gap-10 justify-center'>
       <div className='w-1/3'>
-        <Image src={products[0].imageSrc} alt="" width={640} height={640}/>
+        <Image src={product.imageSrc} alt="" width={640} height={640}/>
       </div>
       <div className='w-1/3'>
-        <h1 className='text-3xl'>{products[0].title}</h1>
-        <p className='text-lg pt-3'>${parseFloat(products[0].variants[0].price).toFixed(2)} USD</p>
+        <h1 className='text-3xl'>{product.title}</h1>
+        {/* <p className='text-lg pt-3'>${parseFloat(products[0].variants[0].price).toFixed(2)} USD</p> */}
         <form>
           <label className="block mb-2 text-slate-400">Size</label>
           <SizeSelector />
@@ -50,7 +90,7 @@ const productPage = ({products, params}) => {
           </div>
           <input type="submit" className="my-2 w-full bg-gray-100 hover:bg-gray-200 text-gray font-bold py-2 px-4 shadow-xl rounded" value="Sold Out"/>
         </form>
-        <p className='p-2'>{products[0].description}</p>
+        <p className='p-2'>{product.description}</p>
       </div>
     </div>
   )
